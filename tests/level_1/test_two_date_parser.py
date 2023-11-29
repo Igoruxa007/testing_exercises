29495acd9df4c27126f0a889e4d318f0418e19f5
Игорь Бородий <Igoruxa007@gmail.com>
2023-11-30 00:51:24 +0300

    fixing problems

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -2,7 +2,15 @@ from functions.level_1.two_date_parser import compose_datetime_from
 import datetime
 
 
-def test_compose_datetime_from():
-    today = datetime.datetime.today()
+def test_compose_datetime_from_date_is_tomorrow():
+    today = datetime.datetime.today() + datetime.timedelta(days=1)
+    assert compose_datetime_from('tomorrow', '16:45') == datetime.datetime(today.year, today.month, today.day, hour=16, minute=45)
+
+def test_compose_datetime_from_date_is_not_tommorow():
+    today = datetime.datetime.today()  
     assert compose_datetime_from('sadf', '16:45') == datetime.datetime(today.year, today.month, today.day, hour=16, minute=45)
-    assert compose_datetime_from('tomorrow', '16:45') == datetime.datetime(today.year, today.month, today.day+1, hour=16, minute=45)
+    assert compose_datetime_from('2023-11-30', '16:45') == datetime.datetime(today.year, today.month, today.day, hour=16, minute=45)
+
+def test_compose_datetime_from_date_is_empty():
+    today = datetime.datetime.today()  
+    assert compose_datetime_from('', '16:45') == datetime.datetime(today.year, today.month, today.day, hour=16, minute=45)
